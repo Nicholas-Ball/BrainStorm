f0e1744929e63881a3a9520d17d15706350a4a6e
Nicholas Ball <89214070+Nicholas-Ball@users.noreply.github.com>
2022-02-26 23:25:44 -0600

    finished Image lib

--- include/img/img.hpp
+++ include/img/img.hpp
@@ -7,44 +7,21 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb/stb_image_write.h"
 
-class Pixel{
-    public:
-        int r,g,b,a;
-        Pixel(int R,int G, int B,int A)
-        {
-            r = R;
-            g = G;
-            b = B;
-            a = A;
-        }
-};
-
-
 class Img{
     private:
         int width, height, channels;
-        std::vector<std::vector<Pixel>> PixelArray;
+        uint8_t* pixels;
 
-        //convert pixel array to stb image
-        stbi_uc* AtoS()
-        {
-            //allocate
-            unsigned char* img = malloc(this->width * this->height * this->channels);
-            if (img == NULL)
-            {
-                std::cout << "Couldn't allocate image" << std::endl;
-                exit(1);
-            }
-
-            for (int i = 0; i != PixelArray.size(); i++)
-            {
-                *img = (uint8_t)();
-             }
-        }
     
     public:
         //constructor
-        Img(){};
+        Img(int x, int y, int comp){
+            width = x;
+            height = y;
+            comp = channels;
+
+            pixels = new uint8_t[height * width * channels];
+        };
 
         //image loader constructor
         Img(std::string file)
@@ -52,46 +29,47 @@ class Img{
             Load(file);
         }
 
-        //load png image from file
+        ~Img() {
+            free(pixels);
+        }
+
+        //load image from file
         void Load(std::string file)
         {
-            this->PixelArray = {};
             //load image
-	        auto img = stbi_load(file.c_str(), &width, &height, &channels, 1);
+	        unsigned char *img = stbi_load(file.c_str(), &this->width, &this->height, &this->channels, 0);
+
             if (img == NULL)
             {
                 std::cout << "Cant't read/find image" << std::endl;
                 exit(1);
             }
 
-            //loop through pixels and add them to array
-            for(int y = 0; y != height;y++)
-            {
-                std::vector<Pixel> xArr;
-                for(int x = 0; x != width; x++)
-                {
-	                unsigned char* pixelOffset = img + (x + width * y) * channels;
-                    xArr.push_back(Pixel(pixelOffset[0],pixelOffset[1],pixelOffset[2],channels >= 4 ? pixelOffset[3] : 0xff));
-                }
-                PixelArray.push_back(xArr);
-            }
+            pixels = img;
+        }
 
-            stbi_image_free(img);
+        //get color value at a pixel
+        int GetColorValue(int x, int y, int channel)
+        {
+            unsigned char* pixelOffset = pixels + (x + (width*y)) * channels;
+            return pixelOffset[channel];
+        }
+
+
+        //set color value at a pixel
+        void SetColorValue(int x, int y, int channel,int value)
+        {
+            *((pixels + (x + (width * y)) * channels) + channel) = (unsigned char)value;
         }
 
 
 
-        //save png image to file (or creates one if one doesn't exist)
+        //save image to file png (or creates one if one doesn't exist)
         void Save(std::string file)
         {
-            stbi_write_png(file.c_str(),this->width,this->height,this->channels,)
+            stbi_write_png(file.c_str(), width, height, channels, pixels, width * channels);
         }
 
-        //returns array of pixels (arr[y][x])
-        std::vector<std::vector<Pixel>> GetArray()
-        {
-            return PixelArray;
-        }
 
         int GetHeight()
         {
--- main.cpp
+++ main.cpp
@@ -5,7 +5,17 @@
 
 
 int main() {
-	Img i("dice.png");
-	std::cout << i.GetWidth() << std::endl;
+	Img i("hill.jpg");
 
+	i.Save("hill1.png");
+
+	std::cout << i.GetColorValue(100, 100, 0) << std::endl;
+	std::cout << i.GetColorValue(100, 100, 1) << std::endl;
+	std::cout << i.GetColorValue(100, 100, 2) << std::endl;
+
+	for (int x = 0; x != i.GetWidth(); x++)
+	{
+		i.SetColorValue(x, 0, 3, 255);
+	}
+	i.Save("hill2.png");
 } 
